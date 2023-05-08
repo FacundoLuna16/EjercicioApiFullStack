327d61e4b6a127bcccc665fa0d5528e5766b094c
Facundo Nicolás José Luna <89422@sistemas.frc.utn.edu.ar>
2023-05-08 19:11:45 -0300

    Tabla series agregada

--- base-ORM/sqlite-init.js
+++ base-ORM/sqlite-init.js
@@ -30,6 +30,27 @@ async function CrearBaseSiNoExiste() {
             ('Los siete samuráis', 'Akira Kurosawa', 1954, 10);`);
     }
 
+    existe = false;
+    res = await db.get("SELECT count(*) as contar FROM sqlite_schema WHERE type = 'table' and name= 'Series'",[]);
+    if (res.contar > 0) existe = true;
+    if (!existe) {
+        await db.run("CREATE table articulosfamilias( IdSerie INTEGER PRIMARY KEY AUTOINCREMENT, Titulo text NOT NULL UNIQUE,Director text NOT NULL, Year text NOT NULL ,CantTemporadas INT, Episodios INT);");
+    console.log("tabla Series creada!");
+    await db.run(
+        `INSERT INTO articulosfamilias (Titulo, Director, Year, CantTemporadas, Episodios) VALUES 
+        ('Juego de Tronos', 'David Benioff y D. B. Weiss', '2011', 8, 73),
+        ('Breaking Bad', 'Vince Gilligan', '2008', 5, 62),
+        ('Friends', 'David Crane y Marta Kauffman', '1994', 10, 236),
+        ('Stranger Things', 'The Duffer Brothers', '2016', 4, 34),
+        ('The Crown', 'Peter Morgan', '2016', 4, 40),
+        ('Narcos', 'Chris Brancato, Carlo Bernard y Doug Miro', '2015', 3, 30),
+        ('House of Cards', 'Beau Willimon', '2013', 6, 73),
+        ('Orange Is the New Black', 'Jenji Kohan', '2013', 7, 91),
+        ('The Big Bang Theory', 'Chuck Lorre y Bill Prady', '2007', 12, 279),
+        ('The Walking Dead', 'Frank Darabont', '2010', 11, 177);`
+    );
+  }
+
     // cerrar la base
     db.close();
 }
