98ee6ee0cc47abb2e0d609ecfde615120f03981b
Facundo Nicolás José Luna <89422@sistemas.frc.utn.edu.ar>
2023-05-08 19:57:42 -0300

    Cambiamos los atributos a tipo DATE

--- base-ORM/sqlite-init.js
+++ base-ORM/sqlite-init.js
@@ -12,42 +12,42 @@ async function CrearBaseSiNoExiste() {
     if (res.contar > 0) existe = true;
     if (!existe) {
         await db.run(
-        "CREATE table Peliculas( IdPelicula INTEGER PRIMARY KEY AUTOINCREMENT, titulo text NOT NULL UNIQUE, director text NOT NULL, year INTEGER NOT NULL, rating INTEGER CHECK (rating >= 0 AND rating <= 10));"
+        "CREATE table Peliculas( IdPelicula INTEGER PRIMARY KEY AUTOINCREMENT, titulo text NOT NULL UNIQUE, director text NOT NULL, year DATE NOT NULL, rating INTEGER CHECK (rating >= 0 AND rating <= 10));"
         );
         console.log("tabla Peliculas creada!");
 
         await db.run(
-            `INSERT INTO Peliculas (titulo, director, year, rating) values 
-            ('El Padrino', 'Francis Ford Coppola', 1972, 9),
-            ('La lista de Schindler', 'Steven Spielberg', 1993, 8),
-            ('El Señor de los Anillos: El Retorno del Rey', 'Peter Jackson', 2003, 10),
-            ('Forrest Gump', 'Robert Zemeckis', 1994, 7),
-            ('El caballero de la noche', 'Christopher Nolan', 2008, 9),
-            ('Star Wars: Episodio IV - Una Nueva Esperanza', 'George Lucas', 1977, 8),
-            ('El silencio de los corderos', 'Jonathan Demme', 1991, 9),
-            ('Pulp Fiction', 'Quentin Tarantino', 1994, 8),
-            ('Volver al futuro', 'Robert Zemeckis', 1985, 7),
-            ('Los siete samuráis', 'Akira Kurosawa', 1954, 10);`);
+            `INSERT INTO Peliculas (titulo, director, year, rating) VALUES
+            ('El Padrino', 'Francis Ford Coppola', '1972-03-24', 9),
+            ('La lista de Schindler', 'Steven Spielberg', '1993-12-15', 8),
+            ('El Señor de los Anillos: El Retorno del Rey', 'Peter Jackson', '2003-12-01', 10),
+            ('Forrest Gump', 'Robert Zemeckis', '1994-07-06', 7),
+            ('El caballero de la noche', 'Christopher Nolan', '2008-07-18', 9),
+            ('Star Wars: Episodio IV - Una Nueva Esperanza', 'George Lucas', '1977-05-25', 8),
+            ('El silencio de los corderos', 'Jonathan Demme', '1991-02-14', 9),
+            ('Pulp Fiction', 'Quentin Tarantino', '1994-09-23', 8),
+            ('Volver al futuro', 'Robert Zemeckis', '1985-07-03', 7),
+            ('Los siete samuráis', 'Akira Kurosawa', '1954-04-26', 10);`);
     }
 
     existe = false;
     res = await db.get("SELECT count(*) as contar FROM sqlite_schema WHERE type = 'table' and name= 'Series'",[]);
     if (res.contar > 0) existe = true;
     if (!existe) {
-        await db.run("CREATE table Series( IdSerie INTEGER PRIMARY KEY AUTOINCREMENT, Titulo text NOT NULL UNIQUE,Director text NOT NULL, Year text NOT NULL ,CantTemporadas INT, Episodios INT);");
+        await db.run("CREATE table Series( IdSerie INTEGER PRIMARY KEY AUTOINCREMENT, Titulo text NOT NULL UNIQUE,Director text NOT NULL, Year DATE NOT NULL ,CantTemporadas INT, Episodios INT);");
     console.log("tabla Series creada!");
     await db.run(
         `INSERT INTO Series (Titulo, Director, Year, CantTemporadas, Episodios) VALUES 
-        ('Juego de Tronos', 'David Benioff y D. B. Weiss', '2011', 8, 73),
-        ('Breaking Bad', 'Vince Gilligan', '2008', 5, 62),
-        ('Friends', 'David Crane y Marta Kauffman', '1994', 10, 236),
-        ('Stranger Things', 'The Duffer Brothers', '2016', 4, 34),
-        ('The Crown', 'Peter Morgan', '2016', 4, 40),
-        ('Narcos', 'Chris Brancato, Carlo Bernard y Doug Miro', '2015', 3, 30),
-        ('House of Cards', 'Beau Willimon', '2013', 6, 73),
-        ('Orange Is the New Black', 'Jenji Kohan', '2013', 7, 91),
-        ('The Big Bang Theory', 'Chuck Lorre y Bill Prady', '2007', 12, 279),
-        ('The Walking Dead', 'Frank Darabont', '2010', 11, 177);`
+        ('Juego de Tronos', 'David Benioff y D. B. Weiss', '2011-04-17', 8, 73),
+        ('Breaking Bad', 'Vince Gilligan', '2008-01-20', 5, 62),
+        ('Friends', 'David Crane y Marta Kauffman', '1994-09-22', 10, 236),
+        ('Stranger Things', 'The Duffer Brothers', '2016-07-15', 4, 34),
+        ('The Crown', 'Peter Morgan', '2016-11-04', 4, 40),
+        ('Narcos', 'Chris Brancato, Carlo Bernard y Doug Miro', '2015-08-28', 3, 30),
+        ('House of Cards', 'Beau Willimon', '2013-02-01', 6, 73),
+        ('Orange Is the New Black', 'Jenji Kohan', '2013-07-11', 7, 91),
+        ('The Big Bang Theory', 'Chuck Lorre y Bill Prady', '2007-09-24', 12, 279),
+        ('The Walking Dead', 'Frank Darabont', '2010-10-31', 11, 177);`
     );
   }
 
--- src/routes/movies.js
+++ src/routes/movies.js
@@ -1,6 +1,6 @@
 
 const {Router} = require('express');
-const _ = require('underscore');
+
 
 
 
