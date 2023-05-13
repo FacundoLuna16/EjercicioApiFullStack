46fdc633ef76519852c26636507758a88e19edb8
FedericoJCocimano <89555@sistemas.frc.utn.edu.ar>
2023-05-13 12:09:23 -0300

    cambio en la linea 52

--- routes/peliculas.js
+++ routes/peliculas.js
@@ -49,7 +49,7 @@ router.put("/:id", async (req, res) => {
       throw error;
     }
 });
-
+// DELETE /api/peliculas/1
 router.delete("/:id", async (req, res) => {
   try {
     let data = await db.peliculas.findOne({
