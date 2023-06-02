4571f483ff6b062cda24a308c0a2c4f729756a62
Matias <90463@sistemas.frc.utn.edu.ar>
2023-06-01 22:29:44 -0300

    test

--- routes/peliculas.js
+++ routes/peliculas.js
@@ -13,41 +13,70 @@ router.get("/:id", async (req, res) => {
   let item = await db.peliculas.findOne({
     where: { IdPelicula: req.params.id}
   });
-  res.json({item});
+  res.json(item);
 });
 
 router.post("/", async (req, res) => {
-  try{
-    let data = await db.peliculas.create({
-      titulo: req.body.titulo,
-      director : req.body.director,
-      year : req.body.year,
-      rating : req.body.rating
+  try {
+    const existingMovie = await db.peliculas.findOne({
+      where: {
+        titulo: req.body.titulo,
+      },
     });
-    res.status(200).json(data.dataValues); // devolvemos el registro agregado!
-  } catch (error){
+
+    if (existingMovie) {
+      // If a movie with the same title already exists, return an error response
+      return res.status(409).json({ error: "Movie with the same title already exists" });
+    }
+
+    const newMovie = await db.peliculas.create({
+      titulo: req.body.titulo,
+      director: req.body.director,
+      year: req.body.year,
+      rating: req.body.rating,
+    });
+
+    res.status(200).json(newMovie.dataValues); // Return the newly added record
+  } catch (error) {
     throw error;
   }
 });
 
 router.put("/:id", async (req, res) => {
   try {
-    let data = await db.peliculas.findOne({
-      where : { IdPelicula: req.params.id }
+    const movieId = req.params.id;
+    let movie = await db.peliculas.findOne({
+      where: { IdPelicula: movieId },
     });
-    if (!data) { 
-      res.status(404).json({ message: "Articulo no encontrado" });
+
+    if (!movie) {
+      res.status(404).json({ message: "Película no encontrada" });
       return;
-      }
-      data.titulo = req.body.titulo,
-      data.director = req.body.director,
-      data.year = req.body.year,
-      data.rating = req.body.rating
-      await data.save();
-      res.sendStatus(200);
-    } catch (error) {
-      throw error;
     }
+
+    const newTitle = req.body.titulo;
+
+    if (newTitle !== movie.titulo) {
+      const existingMovie = await db.peliculas.findOne({
+        where: { titulo: newTitle },
+      });
+
+      if (existingMovie) {
+        res.status(409).json({ message: "El nuevo título ya existe" });
+        return;
+      }
+    }
+
+    movie.titulo = newTitle;
+    movie.director = req.body.director;
+    movie.year = req.body.year;
+    movie.rating = req.body.rating;
+
+    await movie.save()
+    res.sendStatus(200);
+  } catch (error) {
+    throw error;
+  }
 });
 // Con el método DELETE, borramos un registro de la tabla
 router.delete("/:id", async (req, res) => {
--- test/peliculas.test.js
+++ test/peliculas.test.js
@@ -2,18 +2,16 @@ const request = require("supertest");
 const app = require("../index")
 
 const peliculaAlta = {
-    titulo: "Atlas",
+    titulo: "Titulo " + (( ) => (Math.random() + 1).toString(36).substring(2))(),
     director : "Los Wachoski",
-    // year en tipo date
-    year : 2000,
+    year : "2023-05-25",
     rating : 10
 };
 
 const peliculaModificada = {
     id: 1,
-    titulo: "Atlas",
+    titulo:  "Titulo " + (( ) => (Math.random() + 1).toString(36).substring(2))(),
     director : "Los Wachoski",
-    // year en tipo date
     year : 2000,
     rating : 10
 };
@@ -27,11 +25,11 @@ describe("GET /api/peliculas", () => {
         expect(res.body).toEqual(
             expect.arrayContaining([
                 expect.objectContaining({
-                    id: expect.any(Number),
+                    IdPelicula: expect.any(Number),
                     titulo: expect.any(String),
-                    director : expect.any(String),
-                    year : expect.any(Number),
-                    rating : expect.any(Number)
+                    director: expect.any(String),
+                    year: expect.any(String),
+                    rating: expect.any(Number),
                 }),
             ])
         );
@@ -44,59 +42,171 @@ describe("GET /api/peliculas/:id", () => {
         expect(res.statusCode).toEqual(200)
         expect(res.body).toEqual(
                 expect.objectContaining({
-                    id: expect.any(Number),
+                    IdPelicula: expect.any(Number),
                     titulo: expect.any(String),
-                    director : expect.any(String),
-                    year : expect.any(Number),
-                    rating : expect.any(Number)
+                    director: expect.any(String),
+                    year: expect.any(String),
+                    rating: expect.any(Number),
                 }),
         );
     });
 });
 
 describe("POST /api/peliculas", () => {
-    it("deberia crear una pelicula y mostrar la que acaba de crear", async () => {
-        const res = await request(app).post("/api/peliculas").send({peliculaAlta});
-        expect(res.statusCode).toEqual(200);
-        expect(res.body).toEqual(
-            expect.objectContaining({
-                id: expect.any(Number),
-                titulo: expect.any(String),
-                director : expect.any(String),
-                year : expect.any(Number),
-                rating : expect.any(Number)
-            }),
-        );
+    it("should create a movie and return the newly created movie", async () => {
+  
+      // Send a request to create the movie
+      const res = await request(app).post("/api/peliculas").send(peliculaAlta);
+  
+      // Check the response status code
+      expect(res.statusCode).toEqual(200);
+  
+      // Check the response body
+      expect(res.body).toEqual(
+        expect.objectContaining({
+          IdPelicula: expect.any(Number),
+          titulo: peliculaAlta.titulo,
+          director: peliculaAlta.director,
+          year: peliculaAlta.year,
+          rating: peliculaAlta.rating,
+        })
+      );
     });
-});
+  
+    it("should return an error if a movie with the same title already exists", async () => {
+      // Create a movie object with an existing title
+      const existingMovieData = {
+        titulo: "Forrest Gump",
+        director: "Jane Smith",
+        year: "2019",
+        rating: 7,
+      };
+  
+      // Send a request to create a movie with the existing title
+      const res = await request(app).post("/api/peliculas").send(existingMovieData);
+  
+      // Check the response status code
+      expect(res.statusCode).toEqual(409);
+  
+      // Check the error message in the response body
+      expect(res.body).toEqual(
+        expect.objectContaining({
+          error: "Movie with the same title already exists",
+        })
+      );
+    });
+  });
 
 describe("PUT /api/peliculas/:id", () => {
-    it("deberia modificar una pelicula y mostrar la que acaba de modificar", async () => {
-        const res = await request(app).put("/api/peliculas/1").send({peliculaModificada});
-        expect(res.statusCode).toEqual(200);
-        expect(res.body).toEqual(
-            expect.objectContaining({
-                id: expect.any(Number),
-                titulo: expect.any(String),
-                director : expect.any(String),
-                year : expect.any(Number),
-                rating : expect.any(Number)
-            }),
-        );
+    it("debería actualizar los detalles de una película y devolver un estado de éxito", async () => {
+      // Crear una película para actualizar
+      const pelicula = await db.peliculas.create({
+        titulo: "Película antigua",
+        director: "John Doe",
+        year: "2022",
+        rating: 8,
+      });
+  
+      // Datos actualizados de la película
+      const datosPeliculaActualizada = {
+        titulo: "Nueva película",
+        director: "Jane Smith",
+        year: "2023",
+        rating: 9,
+      };
+  
+      // Enviar una solicitud para actualizar la película
+      const res = await request(app)
+        .put(`/api/peliculas/${pelicula.id}`)
+        .send(datosPeliculaActualizada);
+  
+      // Comprobar el código de estado de respuesta
+      expect(res.statusCode).toEqual(200);
+  
+      // Comprobar que los detalles de la película se hayan actualizado en la base de datos
+      const peliculaActualizada = await db.peliculas.findByPk(pelicula.id);
+      expect(peliculaActualizada.titulo).toEqual(datosPeliculaActualizada.titulo);
+      expect(peliculaActualizada.director).toEqual(datosPeliculaActualizada.director);
+      expect(peliculaActualizada.year).toEqual(datosPeliculaActualizada.year);
+      expect(peliculaActualizada.rating).toEqual(datosPeliculaActualizada.rating);
+    });
+  
+    it("debería devolver un error si el nuevo título de la película ya existe", async () => {
+      // Crear dos películas con el mismo título
+      const pelicula1 = await db.peliculas.create({
+        titulo: "Película existente",
+        director: "John Doe",
+        year: "2022",
+        rating: 8,
+      });
+  
+      const pelicula2 = await db.peliculas.create({
+        titulo: "Otra película",
+        director: "Jane Smith",
+        year: "2023",
+        rating: 9,
+      });
+  
+      // Intentar actualizar pelicula1 con el mismo título que pelicula2
+      const datosPeliculaActualizada = {
+        titulo: "Otra película",
+        director: "John Doe",
+        year: "2022",
+        rating: 8,
+      };
+  
+      // Enviar una solicitud para actualizar la película
+      const res = await request(app)
+        .put(`/api/peliculas/${pelicula1.id}`)
+        .send(datosPeliculaActualizada);
+  
+      // Comprobar el código de estado de respuesta
+      expect(res.statusCode).toEqual(409);
+  
+      // Comprobar el mensaje de error en el cuerpo de respuesta
+      expect(res.body).toEqual(
+        expect.objectContaining({
+          message: "El nuevo título ya existe",
+        })
+      );
+    });
+  
+    it("debería devolver un error 404 si el ID de la película no existe", async () => {
+      // Intentar actualizar una película que no existe
+      const datosPeliculaActualizada = {
+        titulo: "Nueva película",
+        director: "John Doe",
+        year: "2022",
+        rating: 8,
+      };
+  
+      // Enviar una solicitud para actualizar la película
+      const res = await request(app).put("/api/peliculas/123456").send(datosPeliculaActualizada);
+  
+      // Comprobar el código de estado de respuesta
+      expect(res.statusCode).toEqual(404);
+  
+      // Comprobar el mensaje de error en el cuerpo de respuesta
+      expect(res.body).toEqual(
+        expect.objectContaining({
+          message: "Película no encontrada",
+        })
+      );
     });
 });
+  
 
 describe("DELETE /api/peliculas/:id", () => {
     it("deberia eliminar una pelicula y mostrar la que acaba de eliminar", async () => {
-        const res = await request(app).delete("/api/peliculas/1");
+        const res = await request(app).delete("/api/peliculas/11");
         expect(res.statusCode).toEqual(200);
         expect(res.body).toEqual(
             expect.objectContaining({
-                id: expect.any(Number),
+                IdPelicula: expect.any(Number),
                 titulo: expect.any(String),
-                director : expect.any(String),
-                year : expect.any(Number),
-                rating : expect.any(Number)
+                director: expect.any(String),
+                year: expect.any(String),
+                rating: expect.any(Number)
             }),
         );
     });
